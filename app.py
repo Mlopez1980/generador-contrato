f813777c2f4f0325b342185b0c39030c3342c427
Mlopez1980 <marco.lopez@hconstructores.com>
2026-06-29 19:18:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # 👉 Marcá aquí los modelos vendidos (no disponibles)
 NO_DISPONIBLES = {
     "1A","1B","1F",
-    "2A","2B","2C","2F",
+    "2A","2C","2F",
     "3A","3B",
     "4A","4B","4C",
     "5A","5B","5D",
